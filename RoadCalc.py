f4fed60326c2a9a2599fc4e4a8ff5f4da4858528
Niki Pourazin <nikipourzin@gmail.com>
2023-07-10 20:44:51 +0330

    Calculate new costs
    Calculate density by paper 2 formula.
    Normalize the density.
    Define a utility function and Implement.

--- RoadCalc.py
+++ RoadCalc.py
@@ -2,7 +2,6 @@ import math
 import networkx as nx
 from matplotlib import pyplot as plt
 import numpy as np
-import scipy as sp
 from scipy import linalg as LA
 
 
@@ -24,22 +23,26 @@ def draw_graph(graph):
 
 
 def find_shortest_path(graph, src, dst):
-    """Dijkstra Alg. is used, to find the SP from src to dst."""
+    """
+    Dijkstra Alg. is used, to find the SP from src to dst.
+    """
 
     return nx.dijkstra_path(graph, source=src, target=dst, weight='weight')
 
 
 def make_normalized_tpm_from_flow_sps_on_intersections(graph, sp_list):
-    """Using the flow count on each path, we calculate the transition possibility from a node to other."""
+    """
+    Using the flow count on each path, we calculate the transition possibility from a node to other.
+    """
 
     # To make the tpm, we will check each sp to count the number of jumps from each node to other
     size = len(graph.nodes)
     probs = [[0 for x in range(size)] for y in range(size)]
-    print(probs)
+    # print(probs)
     for sp in sp_list:
         for k in range(0, (len(sp) - 1)):
             probs[sp[k]][sp[k + 1]] += 1
-    print(probs)
+    # print(probs)
 
     # now sum of each row must be 1. so for non-zeroes, we normalize and if not, the probs[i][i] = 1
     for i in range(size):
@@ -50,15 +53,17 @@ def make_normalized_tpm_from_flow_sps_on_intersections(graph, sp_list):
         else:
             # we have a 0 row
             probs[i][i] = 1.0
-    print(probs)
+    # print(probs)
     return probs
 
 
 def make_dual_graph_and_tpm(graph, tpm):
-    """A graph and it's tpm are given. the function return the dual grapg and it's tpm"""
+    """
+    A graph and it's tpm are given. the function return the dual grapg and it's tpm
+    """
 
     edges = list(graph.edges)
-    print(edges)
+    # print(edges)
     dual_edge_list = []
     new_probs = [[0 for x in range(len(edges))] for y in range(len(edges))]
 
@@ -113,22 +118,24 @@ def find_the_index_of_nearest(array, value):
 
 
 def find_steady_state(tpm):
-    """The steady state for the given TPM is found."""
+    """
+    he steady state for the given TPM is found.
+    """
     # one of the methods is to find eigen values and vectors
     # (A - I)v = 0
     eig_val, eig_vec = LA.eig(tpm, left=True, right=False)
     eig_vec = eig_vec.T
     idx = find_the_index_of_nearest(eig_val, 1)
-
-    print("-----------------------")
-    print(eig_val)
-    print("-----------------------")
-    print(eig_vec)
+    # print(idx)
+    # print("-----------------------")
+    # print(eig_val)
+    # print("-----------------------")
+    # print(eig_vec)
 
     if math.isclose(eig_val[idx], 1, abs_tol=0.001, rel_tol=0.001):
         vec = eig_vec[idx]
-        print(eig_val[idx])
-        print(eig_vec[idx])
+        # print(eig_val[idx])
+        # print(eig_vec[idx])
     else:
         print("No steady state! No eigen value is 1")
         return None
@@ -138,15 +145,74 @@ def find_steady_state(tpm):
         print("No steady state!")
         return None
     else:
-        state = (vec / np.sum(vec))[0]
+        state = (vec / np.sum(vec))
+        for i in range(len(state)):
+            steady_state.append(np.round(state[i].real, 6))
 
     return steady_state
 
 
-def cost_calc(graph, flow_list=None, avg_speed=1, num_lanes=1, num_cars=0):
-    """main run function for iterations. returns the calculated costs on each iter using the CTMC it creates."""
+def calc_road_density(steady_state, num_cars, num_lanes):
+    """
+    First for each road we calculate that how many cars and for how long are in a road.
+    Then, we calculate road capacity by it's length and how many lanes it has.
+    division of this two results in road density over time.
+    """
 
     global road_lens
+
+    den_list = []
+    for i in range(0, len(steady_state)):
+        mass = num_cars * steady_state[i]
+        volume = num_lanes * road_lens[i]
+        den_list.append(mass / volume)
+
+    return den_list
+
+
+def normalize_arr(my_list):
+    if my_list is None or len(my_list) == 0:
+        return my_list
+
+    for i in range(len(my_list)):
+        if max(my_list) != 0:
+            my_list[i] /= max(my_list)
+        else:
+            print("Arr max is 0")
+            return my_list
+
+    return my_list
+
+
+def utilize(graph, den_list, power):
+    """
+    utilization function which multiplies current cost by the mentioned power of corresponding density.
+    That means cost[i] = cost[i] * (d[i]^p)
+    """
+    graph_data = list(graph.edges.data())
+    weights = []
+    for i in range(len(graph_data)):
+        s, d, w = graph_data[i]
+        weights.append(w['weight'])
+    return [weights[i] * (math.pow(den_list[i], power)) for i in range(len(den_list))]
+
+
+def calc_avg_path_cost(w, sp_list):
+    s = 0
+    for sp in sp_list:
+        for k in range(0, (len(sp) - 1)):
+            s += w[(sp[k], sp[k + 1])]
+    return s / len(sp_list)
+
+
+def cost_calc(graph, flow_list=None, avg_speed=1, num_lanes=1, num_cars=0):
+    """
+    main run function for iterations. returns the calculated costs on each iter using the CTMC it creates.
+    each time the new costs are returned along with the new graph that was made with weights.
+    """
+
+    global road_lens
+    global avg_path_cost_list
 
     # Validate input lists
     if flow_list:
@@ -157,7 +223,6 @@ def cost_calc(graph, flow_list=None, avg_speed=1, num_lanes=1, num_cars=0):
     if graph is None:
         return None
 
-    new_edge_costs = []
     sp_list = []
 
     # First, the shortest path for each flow is found
@@ -177,18 +242,40 @@ def cost_calc(graph, flow_list=None, avg_speed=1, num_lanes=1, num_cars=0):
     # normalized travel times once in the main function,
     # Now we use it to fill out the new tpm like paper 1 and 2 proposed.
     new_tpm_with_tt = make_new_tpm_with_tt_values(new_tpm)
-    print(new_tpm_with_tt)
-    # todo: check the previously empty rows later
+    # print(new_tpm_with_tt)
 
     # Find steady states (there might not be any, so maybe(?) approx it).
-    initial_times = find_steady_state(new_tpm_with_tt)
-    print(initial_times)
+    steady_state = find_steady_state(new_tpm_with_tt)
+    if steady_state is None:
+        return None
+    # print(steady_state)
 
-    # TODO calculate utility functions for the state.
+    # Calc road density
+    den_list = calc_road_density(steady_state, num_cars=num_cars, num_lanes=num_lanes)
+    # print(den_list)
+    # colors = np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    # plt.scatter(range(len(den_list)), den_list, c=colors, cmap='viridis')
+    # plt.show()
+    den_list = normalize_arr(den_list)
+    # print(den_list)
 
-    # TODO return new costs for the next iteration.
+    # Define a utility function for the state.
+    # the denser a road is, the higher it's cost should be so it's preferably avoided
+    new_edge_costs = utilize(graph, den_list, 1)
+    # print(new_edge_costs)
 
-    return new_edge_costs
+    new_edges = []
+    weight_map = {}
+    for i in range(len(graph.edges)):
+        s, d = list(graph.edges)[i]
+        weight_map[(s, d)] = new_edge_costs[i]
+        new_edges.append((s, d, new_edge_costs[i]))
+    new_graph = setup_graph(graph.nodes, new_edges)
+
+    avg_path_cost = calc_avg_path_cost(weight_map, sp_list)
+    print(avg_path_cost)
+    avg_path_cost_list.append(avg_path_cost)
+    return new_edge_costs, new_graph
 
 
 if __name__ == '__main__':
@@ -197,7 +284,14 @@ if __name__ == '__main__':
     n = [0, 1, 2, 3, 4, 5]
 
     # Edges of the city graph, the roads.
-    e = [(0, 1, 8), (2, 3, 5), (4, 1, 3), (3, 1, 5), (4, 5, 1), (5, 1, 1), (0, 5, 4), (5, 2, 3)]
+    # e = [(0, 1, 8), (2, 3, 5), (4, 1, 3), (3, 1, 5), (4, 5, 1), (5, 1, 1), (0, 5, 4), (5, 2, 3)]
+    e = [(0, 1, 8), (0, 3, 4),
+         (1, 2, 10), (1, 4, 2),
+         (2, 0, 1), (2, 3, 3),
+         (3, 5, 5),
+         (4, 1, 2), (4, 5, 8),
+         (5, 0, 4), (5, 2, 4)
+         ]
 
     # create city graph
     city_graph = setup_graph(n, e)
@@ -209,11 +303,63 @@ if __name__ == '__main__':
 
     # List of flows in the city, defined by a pair of nodes, indicating src & dst.
     # This list is also hard-coded, could have been taken as input.
-    city_flow_list = [(0, 1), (2, 1), (4, 1), (5, 2)]
+    # city_flow_list = [(0, 1), (2, 1), (4, 1), (5, 2)]
+
+    src_dst = [{'src': 0, 'dst': 1},
+               {'src': 0, 'dst': 2},
+               {'src': 0, 'dst': 2},
+               {'src': 0, 'dst': 3},
+               {'src': 0, 'dst': 3},
+               {'src': 0, 'dst': 3},
+               {'src': 0, 'dst': 3},
+               {'src': 0, 'dst': 4},
+               {'src': 0, 'dst': 4},
+               {'src': 0, 'dst': 5},
+               {'src': 0, 'dst': 5},
+               {'src': 1, 'dst': 0},
+               {'src': 1, 'dst': 0},
+               {'src': 1, 'dst': 2},
+               {'src': 1, 'dst': 3},
+               {'src': 1, 'dst': 4},
+               {'src': 1, 'dst': 5},
+               {'src': 1, 'dst': 5},
+               {'src': 2, 'dst': 0},
+               {'src': 2, 'dst': 0},
+               {'src': 2, 'dst': 1},
+               {'src': 2, 'dst': 3},
+               {'src': 2, 'dst': 4},
+               {'src': 2, 'dst': 5},
+               {'src': 3, 'dst': 0},
+               {'src': 3, 'dst': 1},
+               {'src': 3, 'dst': 4},
+               {'src': 3, 'dst': 5},
+               {'src': 3, 'dst': 5},
+               {'src': 4, 'dst': 0},
+               {'src': 4, 'dst': 0},
+               {'src': 4, 'dst': 0},
+               {'src': 4, 'dst': 0},
+               {'src': 4, 'dst': 1},
+               {'src': 4, 'dst': 2},
+               {'src': 4, 'dst': 3},
+               {'src': 4, 'dst': 5},
+               {'src': 5, 'dst': 0},
+               {'src': 5, 'dst': 1},
+               {'src': 5, 'dst': 1},
+               {'src': 5, 'dst': 2},
+               {'src': 5, 'dst': 2},
+               {'src': 5, 'dst': 3},
+               {'src': 5, 'dst': 3},
+               {'src': 5, 'dst': 3},
+               {'src': 5, 'dst': 4}
+               ]
+    city_flow_list = []
+    for a in src_dst:
+        city_flow_list.append((a['src'], a['dst']))
 
     # Some values are set to be constant, like the average car speed or the number of lanes in roads.
     # These values are hard-coded for the sake of simplicity and again could be input from the prompt if desired.
-    avg_car_speed = 50
+    # avg_car_speed = 50
+    avg_car_speed = 60
     num_of_road_lanes = 1
 
     # Road lengths are to be copied. They are the initial values to road weights.
@@ -226,18 +372,25 @@ if __name__ == '__main__':
     tt = [x / min(tt) for x in tt]
 
     prev_avg_cost = None
+    avg_path_cost_list = []
     while True:
-        new_costs = cost_calc(graph=city_graph, flow_list=city_flow_list, avg_speed=avg_car_speed,
-                              num_lanes=num_of_road_lanes)
+        new_costs, city_graph = cost_calc(graph=city_graph, flow_list=city_flow_list, avg_speed=avg_car_speed,
+                                          num_lanes=num_of_road_lanes)
         if new_costs is None or len(new_costs) == 0:
             print("Something went WRONG")
             break
+
         # The average cost used for checking convergence
         new_avg_cost = sum(new_costs) / len(new_costs)
+        # print(new_avg_cost)
         if prev_avg_cost:
-            # The default no.isclose tolerance is used, could be set o.w. if desired by setting the input parameters.
-            np.isclose(prev_avg_cost, new_avg_cost)
-            break
+            # The np.isclose tolerance could be set o.w. if desired by setting the input parameters.
+            if np.isclose(prev_avg_cost, new_avg_cost, atol=0.001, rtol=0.001):
+                break
         prev_avg_cost = new_avg_cost
 
     print(new_costs)
+    plt.plot(range(len(avg_path_cost_list)), avg_path_cost_list)
+    plt.xlabel('number of iteration')
+    plt.ylabel('avg path cost')
+    plt.show()
